bf3216df5e05e5be8959d40d78370963a6a9cb81
Alex <alex_shu17@mail.ru>
2017-07-30 11:07:08 +0300

    final with gulp

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,8 +11,6 @@ $(document).ready(function(){
             } ]
 
     });
-});
-$(document).ready(function(){
     $('.team__slider').slick({
         dots: false,
         slidesToShow: 3,
@@ -57,43 +55,19 @@ function initMap() {
     });
 
 }
+document.addEventListener('DOMContentLoaded', initMap);
 /* GOOGLE MAP END*/
 /* SMOOTH SCROLL */
 
-let smoothScrollTo = (function () {
-    let timer, start, factor;
-    return function (target, duration) {
-        let offset = window.pageYOffset;
-        let delta  = target - window.pageYOffset; // Y-offset difference
-        duration = duration || 1000;              // default 1 sec animation
-        start = Date.now();                       // get start time
-        factor = 0;
-        if( timer ) {
-            clearInterval(timer); // stop any running animation
-        }
-        function step() {
-            let y;
-            factor = (Date.now() - start) / duration; // get interpolation factor
-            if( factor >= 1 ) {
-                clearInterval(timer); // stop animation
-                factor = 1;           // clip to max 1.0
-            }
-            y = factor * delta + offset;
-            window.scrollBy(0, y - window.pageYOffset);
-        }
-        timer = setInterval(step, 10);
-        return timer; // return the interval timer, so you can clear it elsewhere
-    };
-}());
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.scroll').forEach((scrollLink) => {
-        scrollLink.addEventListener('click', (event) => {
-            let anchor = scrollLink.getAttribute('href');
-            let anchorTo = document.querySelector(anchor);
-            event.preventDefault();
-            smoothScrollTo(anchorTo.offsetTop, 800);
-        });
-    })
+$(document).ready(function(){
+    $('.nav__menu-item a').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1800);
+        e.preventDefault();
+    });
+    return false;
 });
 
 /* SMOOTH SCROLL END*/
